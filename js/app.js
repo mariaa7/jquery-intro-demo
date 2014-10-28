078b55759892689c471e2d1ea69bc733de9a384c
mariaa7 <mariaa7@uw.edu>
2014-10-28 10:06:04 -0700

    Made changes to Main CSS, Index HTML, and App JS

--- js/app.js
+++ js/app.js
@@ -4,3 +4,51 @@
 
 "use strict";
 
+//select all navigation links
+var allNavLinks = $('nav a');
+
+//select all imgs
+var allImgs = $('img');
+
+//select all sections
+var allSections = $('section');
+
+function onReady() {
+
+	//$('nav').slideUp(2000);
+	//$('nav').slideDown();
+	//add attribute of target with value blank to each nav link
+	//allNavLinks.attr('target', '_blank');
+	//allNavLinks.addClass('awesomesauce');
+	allSections.hide();
+
+	
+}
+
+$('#exit-button').dblclick(function() {
+	//show the modal to the world
+	$('#confirm-exit-modal').modal();
+});
+
+$('#confirm-exit-button').click(function() {
+	window.location.href = "http://courses.washington.edu/info343/morris";
+});
+
+allImgs.hover(function() {
+	console.log($(this));
+	$(this).addClass('awesomesauce');
+});
+
+allImgs.mouseout(function() {
+	$(this).removeClass('awesomesauce');
+});
+
+allNavLinks.click(function() {
+
+	$(this).addClass('awesomesauce');
+
+	allSections.hide();
+	$($(this).attr('href')).fadeToggle(1000).addClass('awesomesauce');
+});
+
+$(onReady);
